9400d1bd98fcc2094ef412fed2d587236acf6af4
MNC63 <nloc8380@gmail.com>
2025-06-12 13:39:17 +0700

    fix width countdown rectangle and create turn off alarm

--- Game Python/Clock/Clock.py
+++ Game Python/Clock/Clock.py
@@ -11,9 +11,13 @@ pygame.display.set_caption ("Countdown Timer")
 BLUE_GRAY= (105, 157, 196)
 HARVEST_GOLD = (252, 194, 0)
 BLACK = (0,0,0)
+RED = (255, 0, 0)
 font = pygame.font.Font(None, 36)
 
+
 #load alarm sound
+total_seconds = 0
+alarm_on = False
 alarm_sound = pygame.mixer.Sound("Song_Rest _In_ Peace.mp3")
 
 # Initial time settings (minutes and seconds)
@@ -69,6 +73,21 @@ while True :
     #Draw the clock lines
     pygame.draw.circle(screen, BLACK, (350, 320), 5)
     pygame.draw.line(screen, BLACK, (350,320), (350, 250))
+
+    #calculate current total seconds left
+    current_time = minutes * 60 +seconds
+
+    #calculate width of the red countdown bar based on time
+    if total_seconds > 0:
+        bar_width = int(300 * current_time / total_seconds)
+    else:
+        bar_width = 0
+
+    #Draw the countdown rectangle
+    pygame.draw.rect(screen, BLACK, (100, 440, 304,34))
+    pygame.draw.rect(screen, HARVEST_GOLD, (102, 442, 300,30))
+    pygame.draw.rect(screen, RED, (102, 442, bar_width, 30))
+
     
 
     #Display the current time in MM:SS format
@@ -83,37 +102,49 @@ while True :
             sys.exit()
 
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            mouse_pos = event.pos
-            if BTN_MIN_PLUS.collidepoint(mouse_pos) and not counting_down:
-                minutes = min(minutes +1, 90) # Limit max to 90 minutes
-            elif BTN_MIN_MINUS.collidepoint(mouse_pos) and not counting_down:
-                minutes = max(minutes - 1, 0)
-            elif BTN_SEC_PLUS.collidepoint(mouse_pos) and not counting_down:
-                if seconds == 59:
-                    if minutes <90:
-                        minutes += 1
-                        seconds = 0
+
+            if alarm_on:
+                alarm_sound.stop()
+                pygame.time.set_timer(alarm_stop_event, 0)
+                alarm_on = False
+            else:
+                mouse_pos = event.pos
+                if BTN_MIN_PLUS.collidepoint(mouse_pos) and not counting_down:
+                    minutes = min(minutes +1, 90) # Limit max to 90 minutes
+                    total_seconds = minutes * 60 + seconds 
+                elif BTN_MIN_MINUS.collidepoint(mouse_pos) and not counting_down:
+                    minutes = max(minutes - 1, 0)
+                    total_seconds = minutes * 60 + seconds
+                elif BTN_SEC_PLUS.collidepoint(mouse_pos) and not counting_down:
+                    if seconds == 59:
+                        if minutes <90:
+                            minutes += 1
+                            seconds = 0
+                        else:
+                            seconds = 59
                     else:
-                        seconds = 59
-                else:
-                    seconds = min(seconds + 1, 59)
-            elif BTN_SEC_MINUS.collidepoint(mouse_pos) and not counting_down:
-                if seconds == 0:
-                    if minutes > 0:
-                        minutes -= 1
-                        seconds = 59
-                    else:
-                        seconds = 0
-                else: 
-                    seconds = max(seconds - 1, 0)
-            elif BTN_START.collidepoint(mouse_pos) and not counting_down:
-                if minutes > 0 or seconds > 0:
-                    counting_down = True
-                    pygame.time.set_timer(timer_event, 1000) # set timer to tick every second
-            elif BTN_RESET.collidepoint(mouse_pos):
-                counting_down = False
-                pygame.time.set_timer(timer_event, 0)
-                minutes, seconds = 0, 0
+                        seconds = min(seconds + 1, 59)
+                    total_seconds = minutes * 60 + seconds
+                elif BTN_SEC_MINUS.collidepoint(mouse_pos) and not counting_down:
+                    if seconds == 0:
+                        if minutes > 0:
+                            minutes -= 1
+                            seconds = 59
+                        else:
+                            seconds = 0
+                    else: 
+                        seconds = max(seconds - 1, 0)
+                    total_seconds = minutes * 60 + seconds
+                elif BTN_START.collidepoint(mouse_pos) and not counting_down:
+                    if minutes > 0 or seconds > 0:
+                        counting_down = True
+                        total_seconds = minutes * 60 + seconds # Save total seconds at countdown start
+                        pygame.time.set_timer(timer_event, 1000) # set timer to tick every second
+                elif BTN_RESET.collidepoint(mouse_pos):
+                    counting_down = False
+                    pygame.time.set_timer(timer_event, 0)
+                    minutes, seconds = 0, 0
+                    total_seconds = 0
         
         elif event.type == timer_event and counting_down:
             #count down logic
@@ -122,7 +153,8 @@ while True :
                     counting_down = False
                     pygame.time.set_timer(timer_event, 0)
                     alarm_sound.play()
-                    pygame.time.set_timer(alarm_stop_event, 30000)
+                    alarm_on = True
+                    pygame.time.set_timer(alarm_stop_event, 6000000)
                 else:
                     minutes -= 1
                     seconds = 59
@@ -131,6 +163,7 @@ while True :
         elif event.type == alarm_stop_event:
             alarm_sound.stop()
             pygame.time.set_timer(alarm_stop_event, 0)
+            alarm_on = False
         
     pygame.display.flip()
     clock.tick(30)  # Limit to 30 FPS
